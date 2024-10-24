7b4d97721857af7a51d508d6198975fb5943c655
Krystian Chalimoniuk <krystianchalimoniuk@gmail.com>
2024-10-24 22:02:04 +0200

    drag and drop feature

--- core/ui/src/main/java/pl/nowinkitransferowe/core/ui/NewsResourceCard.kt
+++ core/ui/src/main/java/pl/nowinkitransferowe/core/ui/NewsResourceCard.kt
@@ -16,9 +16,16 @@
 
 package pl.nowinkitransferowe.core.ui
 
+import android.content.ClipData
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.view.View
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.draganddrop.dragAndDropSource
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -46,6 +53,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draganddrop.DragAndDropTransferData
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -77,6 +85,7 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.Locale
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun NewsResourceCardExpanded(
     modifier: Modifier = Modifier,
@@ -90,6 +99,18 @@ fun NewsResourceCardExpanded(
     onTopicClick: (String) -> Unit,
 ) {
     val clickActionLabel = stringResource(R.string.core_ui_card_tap_action)
+    val sharingLabel = stringResource(R.string.core_ui_feed_sharing)
+    val sharingContent = stringResource(
+        R.string.core_ui_feed_sharing_data,
+        userNewsResource.title,
+        userNewsResource.link,
+    )
+
+    val dragAndDropFlags = if (VERSION.SDK_INT >= VERSION_CODES.N) {
+        View.DRAG_FLAG_GLOBAL
+    } else {
+        0
+    }
     val isSelected = highlightSelectedNews && userNewsResource.id == selectedNewsId
 
     OutlinedCard(
@@ -120,8 +141,29 @@ fun NewsResourceCardExpanded(
                     Row {
                         NewsResourceTitle(
                             userNewsResource.title,
-                            modifier = Modifier.fillMaxWidth((.8f)),
+                            modifier = Modifier
+                                .fillMaxWidth((.8f))
+                                .dragAndDropSource {
+                                    detectTapGestures(
+                                        onLongPress = {
+                                            startTransfer(
+                                                DragAndDropTransferData(
+                                                    ClipData.newPlainText(
+                                                        sharingLabel,
+                                                        sharingContent,
+                                                    ),
+                                                    flags = dragAndDropFlags,
+                                                ),
+                                            )
+                                        },
+                                    )
+                                },
                         )
+                        Spacer(modifier = Modifier.weight(1f))
+                        BookmarkButton(isBookmarked, onToggleBookmark)
+
+
+
                         Spacer(modifier = Modifier.weight(1f))
                         BookmarkButton(isBookmarked, onToggleBookmark)
                     }
