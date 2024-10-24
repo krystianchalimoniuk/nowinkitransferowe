670af6a382bc7df2a00fb0c5efe9897e25577155
Krystian Chalimoniuk <krystianchalimoniuk@gmail.com>
2024-10-24 21:25:38 +0200

    Remove implicit `TemporaryFolder` binding with InMemoryDataStore test module

--- core/datastore-test/src/main/java/pl/nowinkitransferowe/core/datastoretest/TestDataStoreModule.kt
+++ core/datastore-test/src/main/java/pl/nowinkitransferowe/core/datastoretest/TestDataStoreModule.kt
@@ -36,26 +36,9 @@ import javax.inject.Singleton
     replaces = [DataStoreModule::class],
 )
 internal object TestDataStoreModule {
-
     @Provides
     @Singleton
     fun providesUserPreferencesDataStore(
-        @ApplicationScope scope: CoroutineScope,
-        userPreferencesSerializer: UserPreferencesSerializer,
-        tmpFolder: TemporaryFolder,
-    ): DataStore<UserPreferences> =
-        tmpFolder.testUserPreferencesDataStore(
-            coroutineScope = scope,
-            userPreferencesSerializer = userPreferencesSerializer,
-        )
-}
-
-fun TemporaryFolder.testUserPreferencesDataStore(
-    coroutineScope: CoroutineScope,
-    userPreferencesSerializer: UserPreferencesSerializer = UserPreferencesSerializer(),
-) = DataStoreFactory.create(
-    serializer = userPreferencesSerializer,
-    scope = coroutineScope,
-) {
-    newFile("user_preferences_test.pb")
+        serializer: UserPreferencesSerializer,
+    ): DataStore<UserPreferences> = InMemoryDataStore(serializer.defaultValue)
 }
